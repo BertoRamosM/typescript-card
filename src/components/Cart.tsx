337c635d652f453d33398079e038c6590dec587e
ARamos <albertramos902@gmail.com>
2024-05-05 19:11:05 +0100

    importing all component types and styles

---
+++ src/components/Cart.tsx
@@ -0,0 +1,13 @@
+import CartItem from "./CartItem";
+
+import { Wrapper } from "./Cart.styles";
+
+import { CartItemType } from "../App";
+
+const Cart: React.FC = () => {
+  return (
+    <CartItem />
+  )
+}
+
+export default Cart
