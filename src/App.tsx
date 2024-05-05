367b0a40c94d8529879fccb7ab5c0a971fa051b8
ARamos <albertramos902@gmail.com>
2024-05-05 16:40:19 +0100

    installed materialui, react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+import { useQuery } from "react-query"
 
-import './App.css'
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid"
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/badge'
+
+
+
 
 function App() {
 
   return (
     <>
-      
+      <h1>Cart</h1>
     </>
   )
 }
