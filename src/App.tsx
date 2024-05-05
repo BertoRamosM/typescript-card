fcf24a14a30764b1a26fb792e9239d38a710cb27
ARamos <albertramos902@gmail.com>
2024-05-05 16:50:21 +0100

    react query to fetch data created

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,28 @@ import Grid from "@material-ui/core/Grid"
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/badge'
 
+import { Wrapper } from "./App.styles";
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
 
+const getProducts = async (): Promise<CartItemType[]> => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  return data;
+};
 
 function App() {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts)
+
+  const getTota
 
   return (
     <>
